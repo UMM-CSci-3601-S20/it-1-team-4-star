cfbae79d86ed4b5fd9669fd1f997861db6ca3e09
Johannes Martinez <mart4695@morris.umn.edu>
2020-03-05 16:42:19 -0600

    Add validators to add.user.component.ts
    Epic #1. Story #12.

--- client/src/app/users/add-user.component.ts
+++ client/src/app/users/add-user.component.ts
@@ -30,28 +30,21 @@ export class AddUserComponent implements OnInit {
       {type: 'existingName', message: 'Name has already been taken'}
     ],
 
-    age: [
-      {type: 'pattern', message: 'Age must be a number'},
-      {type: 'min', message: 'Age must be at least 15'},
-      {type: 'max', message: 'Age may not be greater than 200'},
-      {type: 'required', message: 'Age is required'}
-    ],
-
     email: [
       {type: 'email', message: 'Email must be formatted properly'},
       {type: 'required', message: 'Email is required'}
     ],
 
-    role: [
-      { type: 'required', message: 'Role is required' },
-      { type: 'pattern', message: 'Role must be Admin, Editor, or Viewer' },
+    building: [
+      {type: 'required', message: 'Office number is required'},
+      {type: 'minlength', message: 'Office number must be at least 2 characters long'},
+      {type: 'pattern', message: 'Office number must contain only numbers'}
     ],
 
     officeNumber: [
       {type: 'required', message: 'Office number is required'},
       {type: 'minlength', message: 'Office number must be at least 2 characters long'},
-      {type: 'pattern', message: 'Office number must contain only numbers'},
-      {type: 'existingName', message: 'Office number has already been taken'}
+      {type: 'pattern', message: 'Office number must contain only numbers'}
     ]
   };
 
@@ -78,18 +71,6 @@ export class AddUserComponent implements OnInit {
         },
       ])),
 
-      // Since this is for a company, we need workers to be old enough to work, and probably not older than 200.
-      age: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[0-9]+[0-9]?'),
-        Validators.min(15),
-        Validators.max(200),
-      ])),
-
-      // We don't care much about what is in the company field, so we just add it here as part of the form
-      // without any particular validation.
-      company: new FormControl(),
-
       // We don't need a special validator just for our app here, but there is a default one for email.
       // We will require the email, though.
       email: new FormControl('', Validators.compose([
@@ -97,10 +78,18 @@ export class AddUserComponent implements OnInit {
         Validators.email,
       ])),
 
-      role: new FormControl('viewer', Validators.compose([
+      building: new FormControl('', Validators.compose([
         Validators.required,
-        Validators.pattern('^(admin|editor|viewer)$'),
+        Validators.minLength(2),
+        Validators.pattern('^[A-Za-z0-9\\s]+[A-Za-z0-9\\s]+$(\\.0-9+)?'),
       ])),
+
+      officeNumber: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern('^[A-Za-z0-9\\s]+[A-Za-z0-9\\s]+$(\\.0-9+)?'),
+      ])),
+
     });
 
   }
