28f9665081ec26aff86fd3ebf54d2df90c61a30a
Cookie Vang <vang2660@morris.umn.edu>
2020-03-10 15:16:48 -0500

    Take out duplicate imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,8 +32,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { AddUserComponent } from './users/add-user.component';
-import { NoteCardComponent } from './notes/note-card.component';
-import { NoteListComponent } from './notes/note-list.component';
+
 
 import { NoteListComponent } from './notes/note-list.component';
 import { NoteService } from './notes/note.service';
