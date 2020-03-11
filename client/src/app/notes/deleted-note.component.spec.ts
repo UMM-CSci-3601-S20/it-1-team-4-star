192b8b7a6ef61e239ff577084d2acbb8403af55f
Firas <naber024@morris.umn.edu>
2020-03-11 16:15:00 -0500

    Add deleted note files

---
+++ client/src/app/notes/deleted-note.component.spec.ts
@@ -0,0 +1,130 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockNoteService } from '../../testing/note.service.mock';
+import { Note } from './note';
+
+import { NoteCardComponent } from './note-card.component';
+import { DeletedNoteComponent } from './deleted-note.component';
+import { NoteService } from './note.service';
+import { MatIconModule } from '@angular/material/icon';
+
+const COMMON_IMPORTS: any[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatIconModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
+
+describe('Deleted note', () => {
+
+  let deletedNote: DeletedNoteComponent;
+  let fixture: ComponentFixture<DeletedNoteComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [DeletedNoteComponent, NoteCardComponent],
+      // providers:    [ NoteService ]  // NO! Don't provide the real service!
+      // Provide a test-double instead
+      providers: [{ provide: NoteService, useValue: new MockNoteService() }]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(DeletedNoteComponent);
+      deletedNote = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  // these test from the mock note service
+
+  it('contains all the notes', () => {
+    expect(deletedNote.serverFilteredNotes.length).toBe(3);
+  });
+
+  it('contains a note owner with the name \'Rachel Johnson\'', () => {
+    expect(deletedNote.serverFilteredNotes.some((note: Note) => note.owner === 'Rachel Johnson')).toBe(true);
+  });
+
+  it('ontains a note owner with the name \'Nic McPhee\'', () => {
+    expect(deletedNote.serverFilteredNotes.some((note: Note) => note.owenr === 'Nic McPhee')).toBe(true);
+  });
+
+  it('doesn\'t contain a note named \'John Cena\'', () => {
+    expect(deletedNote.serverFilteredNotes.some((note: Note) => note.owner === 'John Cena')).toBe(false);
+  });
+
+  /* it('has two notes that are in the same building', () => {
+    expect(deletedNote.serverFilteredNotes.filter((note: Note) => note.body === 'Science').length).toBe(2);
+  }); */
+});
+
+describe('Misbehaving Deleted Note', () => {
+  let deletedNote: DeletedNoteComponent;
+  let fixture: ComponentFixture<DeletedNoteComponent>;
+
+  let noteServiceStub: {
+    getNotes: () => Observable<Note[]>;
+    getNotesFiltered: () => Observable<Note[]>;
+  };
+
+  beforeEach(() => {
+    // stub NoteService for test purposes
+    noteServiceStub = {
+      getNotes: () => new Observable(observer => {
+        observer.error('Error-prone observable');
+      }),
+      getNotesFiltered: () => new Observable(observer => {
+        observer.error('Error-prone observable');
+      })
+    };
+
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [DeletedNoteComponent],
+      // providers:    [ NoteService ]  // NO! Don't provide the real service!
+      // Provide a test-double instead
+      providers: [{ provide: NoteService, useValue: noteServiceStub }]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(DeletedNoteComponent);
+      deletedNote = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('generates an error if we don\'t set up a DeletedNoteService', () => {
+    // Since the observer throws an error, we don't expect notes to be defined.
+    expect(deletedNote.serverFilteredNotes).toBeUndefined();
+  });
+});
---
+++ client/src/app/notes/deleted-note.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Note } from './note';
+import { NoteService } from './note.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-note-list-component',
+  templateUrl: 'deleted-note.component.html',
+  styleUrls: ['./deleted-note.component.scss'],
+  providers: []
+})
+
+export class DeletedNoteComponent implements OnInit, OnDestroy  {
+  // These are public so that tests can reference them (.spec.ts)
+
+  public serverFilteredNotes: Note[];
+  public filteredNotes: Note[];
+
+  public owner: string;
+  public body: string;
+  public reusable: boolean;
+  public draft: boolean;
+  public toDelete: boolean;
+
+  public viewType: 'card' | 'list' = 'card';
+  getDeletedNotesSub: Subscription;
+
+
+
+  // Inject the NoteService into this component.
+  // That's what happens in the following constructor.
+  //
+  // We can call upon the service for interacting
+  // with the server.
+
+  constructor(private noteService: NoteService) {
+
+  }
+
+  getDeletedNotesFromServer(): void {
+    this.unsub();
+    this.getDeletedNotesSub = this.noteService.getDeletedNotes({
+      owner: this.owner,
+      body: this.body
+    }).subscribe(returnedNotes => {
+      this.serverFilteredNotes = returnedNotes;
+      this.updateFilter();
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  public updateFilter(): void {
+    this.filteredNotes = this.noteService.filterNotes(
+      this.serverFilteredNotes, { owner: this.owner, body: this.body, reusable: this.reusable, draft: this.draft,
+        toDelete: this.toDelete });
+  }
+
+  /**
+   * Starts an asynchronous operation to update the notes list
+   *
+   */
+  ngOnInit(): void {
+    this.getDeletedNotesFromServer();
+  }
+
+
+  ngOnDestroy(): void {
+    this.unsub();
+  }
+
+  unsub(): void {
+    if (this.getDeletedNotesSub) {
+      this.getDeletedNotesSub.unsubscribe();
+    }
+  }
+}
