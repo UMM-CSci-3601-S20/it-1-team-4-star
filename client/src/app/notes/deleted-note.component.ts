63b82880d2cad488a93b3b3da77303fbd8937f30
Johannes Martinez <mart4695@morris.umn.edu>
2020-03-12 12:21:02 -0500

    Remove references to card vs list display in deleted-note.component.ts

--- client/src/app/notes/deleted-note.component.ts
+++ client/src/app/notes/deleted-note.component.ts
@@ -4,7 +4,7 @@ import { NoteService } from './note.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-deleted-note-list-component',
+  selector: 'deleted-note.component',
   templateUrl: 'deleted-note.component.html',
   styleUrls: ['./deleted-note.component.scss'],
   providers: []
@@ -15,25 +15,29 @@ export class DeletedNoteComponent implements OnInit, OnDestroy  {
   public serverFilteredNotes: Note[];
   public filteredNotes: Note[];
   public body: string;
-  public reusable: boolean;
+  public reuse: boolean;
   public draft: boolean;
   public toDelete: boolean;
-  public viewType: 'card' | 'list' = 'card';
-  getDeletedNotesSub: Subscription;
+  getNotesSub: Subscription;
+
 
   // Inject the NoteService into this component.
   // That's what happens in the following constructor.
   //
   // We can call upon the service for interacting
   // with the server.
+
   constructor(private noteService: NoteService) {
 
   }
 
-  getDeletedNotesFromServer(): void {
+  getNotesFromServer(): void {
     this.unsub();
-    this.getDeletedNotesSub = this.noteService.getNotes({
-      body: this.body
+    this.getNotesSub = this.noteService.getNotes({
+      body: this.body,
+      reuse: this.reuse,
+      draft: this.draft,
+      toDelete: this.toDelete
     }).subscribe(returnedNotes => {
       this.serverFilteredNotes = returnedNotes;
       this.updateFilter();
@@ -41,23 +45,27 @@ export class DeletedNoteComponent implements OnInit, OnDestroy  {
       console.log(err);
     });
   }
+
   public updateFilter(): void {
     this.filteredNotes = this.noteService.filterNotes(
-      this.serverFilteredNotes, {body: this.body, reuse: this.reusable, draft: this.draft, toDelete: this.toDelete });
+      this.serverFilteredNotes, {body: this.body, reuse: this.reuse = true, draft: this.draft = true, toDelete: this.toDelete = false});
   }
+
   /**
-   * Starts an asynchronous operation to update the notes list
+   * Starts an asynchronous operation to update the notes reuse
    *
    */
   ngOnInit(): void {
-    this.getDeletedNotesFromServer();
+    this.getNotesFromServer();
   }
+
   ngOnDestroy(): void {
     this.unsub();
   }
+
   unsub(): void {
-    if (this.getDeletedNotesSub) {
-      this.getDeletedNotesSub.unsubscribe();
+    if (this.getNotesSub) {
+      this.getNotesSub.unsubscribe();
     }
   }
 }
