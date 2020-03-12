72d2aaea4a3a02ce529e75187458afe273c09d05
Johannes Martinez <mart4695@morris.umn.edu>
2020-03-11 22:29:30 -0500

    Comment out test in note-card-component.spec in preparation of better tests
    #15.

--- client/src/app/notes/note-card.component.spec.ts
+++ client/src/app/notes/note-card.component.spec.ts
@@ -33,7 +33,7 @@ describe('NoteCardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
